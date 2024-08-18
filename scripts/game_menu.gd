5d3718b5711286b5d50b68c75803ca642544986f
CreeperXD <48917115+CreeperXD@users.noreply.github.com>
2024-08-18 16:13:23 +0800

    implemented pause button

--- scripts/game_menu.gd
+++ scripts/game_menu.gd
@@ -2,6 +2,7 @@ extends CanvasLayer
 
 func _ready() -> void:
 	$VictoryMenu.hide()
+	$PauseMenu.hide()
 
 func _process(delta: float) -> void:
 	pass
@@ -9,5 +10,24 @@ func _process(delta: float) -> void:
 func show_victory_menu() -> void:
 	$VictoryMenu.show()
 
+func hide_pause_button() -> void:
+	$PauseButton.hide()
+
 func _on_next_level_button_pressed() -> void:
 	SceneManager.change_to_next_scene()
+
+func _on_pause_button_pressed() -> void:
+	get_tree().paused = true
+	$PauseMenu.show()
+
+func _on_restart_button_pressed() -> void:
+	get_tree().paused = false
+	get_tree().reload_current_scene()
+
+func _on_main_menu_button_pressed() -> void:
+	get_tree().paused = false
+	SceneManager.change_scene(0)
+
+func _on_resume_button_pressed() -> void:
+	get_tree().paused = false
+	$PauseMenu.hide()
